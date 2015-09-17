18c117a81cffbbfe594faed09b907deea533af30
elecom <dacostaelecom@gmail.com>
2015-09-16 21:25:53 -0430

    Modificacion y actualizacion de codigos fuentes.

--- public/js/app/tomapedido.js
+++ public/js/app/tomapedido.js
@@ -7,7 +7,7 @@ configextra = function(){
     var tabla = $('#tab_datosproductos');
     
     $('#txtBusqueda').on('keyup', function(e){
-        $.uiTableFilter(tabla, this.value, null, false);
+        $.uiTableFilter(tabla, this.value);
     });
 };
 
@@ -17,47 +17,6 @@ inicio = function(){
     
     configextra();
     
-    /*$.ajax({
-        url:'mostrarProductos',
-        type: 'POST',
-        dataType: 'json',
-        success: function(data){
-            $('#tab_datospedidos_pruebas').dataTable({
-               scrollY: '300px',
-               scrollCollapse: true,
-               paging: true,
-               pagingType:'full_numbers',
-               data: data['productos'],
-               language:{
-                   decimal: ",",
-                   thousands: ".",
-                   lengthMenu: 'Mostrar _MENU_ por paginas',
-                   zeroRecords: 'No hay datos disponibles'
-               },
-               info: false,
-               ordering: false,
-               columns:[
-                   {'data':'Codigo'},
-                   {'data':'Nombre'},
-                   {'data':'Precio'},
-                   {'data':'Existencia'},
-                   {'data':'Vencimiento'}
-               ],
-               deferRender: true,
-               order:[[0, 'asc']]
-            });
-            
-            $('#tab_datospedidos_pruebas2').dataTable({
-                searching: false,
-                info: false,
-                paging: false,
-                ordering: false,
-                language:{
-                    zeroRecords: 'No hay datos disponibles'
-                }
-            });
-        }
-    });*/
     $.ajax({
         url: 'mostrarProductos',
         type:'POST',
@@ -81,13 +40,9 @@ inicio = function(){
                     )
                     .append(
                         $('<td>', {style:'border: 1px solid #D0E5F5;', class:'centrado'})
-                        /*.append($('<input>', {id:'cant-'+item.Codigo, type:'number', class:'der', style:'width:80px;', min:item.UMF, max:item.Existencia, value:item.UMF, step:item.UMF}))*/
                         .append($('<input>', {id:'sele-'+item.Codigo, type:'button', class:'boton-click', value:'+', title:'Agregar el producto al carrito de compra'}).on('click', function(){
-                            //agregarItem(this.id, $('#cant-'+item.Codigo).val(), $('#prec-'+item.Codigo).text(), $('#desc-'+item.Codigo).text(), item.UMF, $('#exis-'+item.Codigo).text());
+                            
                         }))
-                        /*.append($('<input>', {id:'qite-'+item.Codigo, type:'button', class:'boton-click2 ocultar', title:'Quitar producto del carrito', style:'margin:0 auto;'}).on('click', function(){
-                            quitarItem(this.id, item.UMF);
-                        }))*/
                     )
                 );
             });
